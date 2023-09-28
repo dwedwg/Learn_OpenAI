07311e62c4efdd0acc2533c83aaa3265b779620c
dwedwg <abhiram.bondada@wsu.edu>
2023-09-27 20:31:03 -0700

    Update web_crawl_embed.py

--- web_crawl_embed.py
+++ web_crawl_embed.py
@@ -16,7 +16,7 @@ from ast import literal_eval
 # Regex pattern to match a URL
 HTTP_URL_PATTERN = r'^http[s]{0,1}://.+$'
 
-openai.api_key = 'OPENAIKEY'
+openai.api_key = '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'
 
 # Define URL
 domain = "microvision.com"
@@ -158,11 +158,9 @@ list_of_texts=[]
 
 for file in os.listdir("text/" + domain + "/"):
 
-    # Open the file and read the text
     with open("text/" + domain + "/" + file, "r", encoding="UTF-8") as f:
         text = f.read()
 
-        # Omit the first 11 lines and the last 4 lines, then replace -, _, and #update with spaces.
         texts.append((file[11:-4].replace('-',' ').replace('_', ' ').replace('#update',''), text))
 
 df = pd.DataFrame(list_of_texts, columns = ['fname', 'text'])
@@ -187,37 +185,27 @@ max_tokens = 500
 
 def split_into_many(text, max_tokens = max_tokens):
 
-    # Split the text into sentences
     sentences = text.split('. ')
 
-    # Get the number of tokens for each sentence
     n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
     
     chunks = []
     tokens_so_far = 0
     chunk = []
 
-    # Loop through the sentences and tokens joined together in a tuple
     for sentence, token in zip(sentences, n_tokens):
 
-        # If the number of tokens so far plus the number of tokens in the current sentence is greater 
-        # than the max number of tokens, then add the chunk to the list of chunks and reset
-        # the chunk and tokens so far
         if tokens_so_far + token > max_tokens:
             chunks.append(". ".join(chunk) + ".")
             chunk = []
             tokens_so_far = 0
 
-        # If the number of tokens in the current sentence is greater than the max number of 
-        # tokens, go to the next sentence
         if token > max_tokens:
             continue
 
-        # Otherwise, add the sentence to the chunk and add the number of tokens to the total
         chunk.append(sentence)
         tokens_so_far += token + 1
         
-    # Add the last chunk to the list of chunks
     if chunk:
         chunks.append(". ".join(chunk) + ".")
 
